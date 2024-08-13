d7c59d08b7c9774278cbc76c18795b9d138dc302
Lachlan Leone <starfluxgames@gmail.com>
2024-08-13 11:02:08 +1000

    Update 0.1.1

--- Mod.cs
+++ Mod.cs
@@ -11,9 +11,9 @@ namespace SmallerFlames
     {
         public const string MOD_GUID = "com.starfluxgames.smallerflames";
         public const string MOD_NAME = "Smaller Flames";
-        public const string MOD_VERSION = "0.1.0";
-        public const string MOD_AUTHOR = "My Name";
-        public const string MOD_GAMEVERSION = ">=1.1.4";
+        public const string MOD_VERSION = "0.1.1";
+        public const string MOD_AUTHOR = "StarFluxGames";
+        public const string MOD_GAMEVERSION = ">=1.2.0";
 
         public Mod() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, MOD_GAMEVERSION, Assembly.GetExecutingAssembly()) { }
 
@@ -37,7 +37,7 @@ namespace SmallerFlames
     {
         static void Postfix(ApplianceView __instance, ApplianceView.ViewData view_data)
         {
-            GameObject Fire = __instance.transform.Find("Fire").gameObject;
+            GameObject Fire = __instance.transform.Find("VFX Manager").Find("Fire").gameObject;
             if (Fire != null)
             {
                 Fire.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
